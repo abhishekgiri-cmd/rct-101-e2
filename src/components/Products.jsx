a9c54beb5ef2dc84fa60a1b4f66ce1619f28234b
Abhishek Giri <70642966+abhishekgiri-cmd@users.noreply.github.com>
2022-05-30 16:45:04 +0530

    last commit

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,15 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Flex, Grid } from "@chakra-ui/react";
+import axios from "axios";
+
+import Product from "./Product";
+import AddProduct from "./AddProduct";
+import Pagination from "./Pagination";
 
 const Products = () => {
-  // TODO: Remove below const and instead import them from chakra
-  const Flex = () => <div />;
-  const Grid = () => <div />;
+  const [pageFilter, setPageFilter] = useState({
+    pageNumber: 1,
+    limit: 3,
+    totalCount: 0,
+  });
+  const [products, setProducts] = useState([]);
 
+  const { pageNumber, limit } = pageFilter;
+
+  const handleOnAdd = (body) => {
+    axios.post("http://localhost:8080/products", body).then((r) => {
+      setProducts([...products, r.data]);
+    });
+  };
+  useEffect(() => {
+    const getProducts = async () => {
+      const response = await axios.get(
+        `http://localhost:8080/products?_page=${pageNumber}&_limit=${limit}`
+      );
+      setPageFilter({
+        ...pageFilter,
+      
+      });
+
+      setProducts(response.data);
+    };
+    getProducts();
+  }, [pageNumber, limit]);
   return (
-    <Flex>
-      {/*  AddProduct */}
-      <Grid>{/* List of Products */}</Grid>
-      {/* Pagination */}
+    <Flex
+    direction="column"
+    justifyContent="center"
+    alignItems="center"
+    borderRadius={50}
+   
+    >
+      <AddProduct add={handleOnAdd} />
+      <Grid  gridGap={4} gridTemplateColumns="300px 300px 300px">
+        {products.map((p) => (
+          <Product key={p.id} {...p} />
+        ))}
+      </Grid>
+      <Pagination pageFilter={pageFilter} setPageFilter={setPageFilter} />
     </Flex>
   );
 };
